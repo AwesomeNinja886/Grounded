944e8bb7622140cdc7fdf7892984f5742d0314ad
AwesomeNinja886 <awesomeninja886@gmail.com>
2023-03-15 17:10:09 -0400

    it finally works

--- src/main/java/com/awesomeninja/grounded/datagen/ModRecipeProvider.java
+++ src/main/java/com/awesomeninja/grounded/datagen/ModRecipeProvider.java
@@ -4,6 +4,7 @@ import java.util.function.Consumer;
 
 import com.awesomeninja.grounded.block.ModBlocks;
 
+import net.minecraft.advancements.critereon.ItemPredicate;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.data.recipes.RecipeProvider;
@@ -22,6 +23,7 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
             .define('M', ModBlocks.CRIMSON_MUD.get())
             .pattern("MM")
             .pattern("MM")
+            .unlockedBy("has_crimson_mud", inventoryTrigger(ItemPredicate.Builder.item().of(ModBlocks.CRIMSON_MUD.get()).build()))
             .save(pFinishedRecipeConsumer);
     }
 }

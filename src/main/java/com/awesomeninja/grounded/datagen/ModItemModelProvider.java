02690227c923e93e84843a59ff469f0255a8a75d
AwesomeNinja886 <awesomeninja886@gmail.com>
2023-03-16 21:44:21 -0400

    not sure why this isn't working

--- src/main/java/com/awesomeninja/grounded/datagen/ModItemModelProvider.java
+++ src/main/java/com/awesomeninja/grounded/datagen/ModItemModelProvider.java
@@ -1,32 +1,35 @@
 package com.awesomeninja.grounded.datagen;
 
-import com.awesomeninja.grounded.item.ModItems;
+import com.awesomeninja.grounded.Grounded;
+import com.awesomeninja.grounded.block.ModBlocks;
 
 import net.minecraft.data.DataGenerator;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.Item;
-import net.minecraftforge.client.model.generators.ItemModelBuilder;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
-import net.minecraftforge.registries.RegistryObject;
 
 public class ModItemModelProvider extends ItemModelProvider {
 
-    public ModItemModelProvider(DataGenerator generator, String modid, ExistingFileHelper existingFileHelper) {
-        super(generator, modid, existingFileHelper);
+    public ModItemModelProvider(DataGenerator generator, ExistingFileHelper existingFileHelper) {
+        super(generator, Grounded.MODID, existingFileHelper);
     }
 
     @Override
     protected void registerModels() {
-        for (RegistryObject<Item> entry : ModItems.ITEMS.getEntries()) {
-            if (entry.get() instanceof BlockItem blockItem) {
-                block(blockItem);
-            }
-        }
-    }
+        withExistingParent(ModBlocks.CRIMSON_MUD.get().getDescriptionId(), new ResourceLocation(Grounded.MODID, "block/block.grounded.crimson_mud"));
+        withExistingParent(ModBlocks.POLISHED_CRIMSON_MUD.get().getDescriptionId(), new ResourceLocation(Grounded.MODID,"block/block.grounded.polished_crimson_mud"));
 
-    protected ItemModelBuilder block(BlockItem blockItem) {
-        return withExistingParent(blockItem.getDescriptionId(),modid + ":block/" + blockItem.getBlock().getDescriptionId());
+        withExistingParent(ModBlocks.WARPED_LOAM.get().getDescriptionId(), new ResourceLocation(Grounded.MODID,"block/block.grounded.warped_loam"));
+        withExistingParent(ModBlocks.LOAM_WARPED_NYLIUM.get().getDescriptionId(), new ResourceLocation(Grounded.MODID,"block/block.grounded.loam_warped_nylium"));
+        withExistingParent(ModBlocks.WARPED_LOAM_BRICKS.get().getDescriptionId(), new ResourceLocation(Grounded.MODID,"block/block.grounded.warped_loam_bricks"));
+
+        withExistingParent(ModBlocks.ENTRAILITE.get().getDescriptionId(), new ResourceLocation(Grounded.MODID,"block/entrailite"));
+        withExistingParent(ModBlocks.ENTRAILITE_CRIMSON_NYLIUM.get().getDescriptionId(), new ResourceLocation(Grounded.MODID,"block/block.grounded.entrailite_crimson_nylium"));
+        withExistingParent(ModBlocks.ENTRAILITE_WARPED_NYLIUM.get().getDescriptionId(), new ResourceLocation(Grounded.MODID,"block/block.grounded.entrailite_warped_nylium"));
+        withExistingParent(ModBlocks.COBBLED_ENTRAILITE.get().getDescriptionId(), new ResourceLocation(Grounded.MODID,"block/block.grounded.cobbled_entrailite"));
+        withExistingParent(ModBlocks.SMOOTH_ENTRAILITE.get().getDescriptionId(), new ResourceLocation(Grounded.MODID,"block/block.grounded.smooth_entrailite"));
+        withExistingParent(ModBlocks.LAYERED_ENTRAILITE.get().getDescriptionId(), new ResourceLocation(Grounded.MODID,"block/block.grounded.layered_entrailite"));
+        withExistingParent(ModBlocks.POLISHED_ENTRAILITE.get().getDescriptionId(), new ResourceLocation(Grounded.MODID,"block/block.grounded.polished_entrailite"));
     }
     
 }
